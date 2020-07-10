3d9d302dbb01be221bc4881b92fbd9ec324ed3a9
PilzAdam <PilzAdam@minetest.net>
2020-07-10 23:35:40 +0200

    More style improvements
    * replace == with =
    * use readonly
    * set nounset, errexit, and pipefail
    * use sneak_case for variable names
    * use "here document" for help text
    * use -eq for numbers instead of =
    * introduce failure and warning messages
    * use constants for colors

--- simple-restart-check.sh
+++ simple-restart-check.sh
@@ -5,7 +5,7 @@
 # The main idea to is to check in /proc/$pid/maps for entries marked as executable and "(deleted)".
 # See below for a list of 'ignore patterns', for which we don't care if they are marked as "(deleted)"
 
-IGNORE_PATTERNS=(
+readonly ignore_patterns=(
 	"/dev/*"         # device files
 	"/run/*"         # temporary run-time files
 	"/var/run/*"
@@ -13,12 +13,40 @@ IGNORE_PATTERNS=(
 	"/tmp/.gl*"      # temporary OpenGL (?) files, e.g. /tmp/.glWSsluM
 )
 
+readonly c_process_name="\e[0;35m" # color used for process name output
+readonly c_process_exe="\e[0;33m"  # color used for exe name when it differs from comm
+readonly c_pid="\e[0;1m"           # color used for the PID
+readonly c_reset="\e[0m"           # resets color to normal
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
 pids=() # PIDs to check
 verbose="" # whether to print outdated library names if there are more than 1 for a process
 fullpath="" # whether to print the full path of libraries or just the filename
 
 function usage {
 	echo "Usage: $(basename "$0") [-p PID]... [-v] [-f] [-h]" 1>&2
+}
+
+# Some helper functions for logging progress
+function print_progress {
+	echo -n "[$1/$2]" >&2
+}
+function clear_line {
+	echo -en "\r             \r" >&2
+}
+
+function fail {
+	clear_line
+	echo "ERROR: $*" 1>&2
+	exit 2
+}
+
+function warn {
+	clear_line
+	echo "WARNING: $*" 1>&2
 }
 
 while getopts ":p:vfh" opt
@@ -29,26 +57,29 @@ do
 		f) fullpath="1" ;;
 		h)
 			usage
-			echo "Checks which currently running processes use outdated libraries." 1>&2
-			echo "" 1>&2
-			echo "Options:" 1>&2
-			echo "  -p PID   Only check the process with the given PID. Can be given multiple" 1>&2
-			echo "           times, in which case all explicitly given processes are checked." 1>&2
-			echo "  -v       List all outdated libraries for each process." 1>&2
-			echo "  -f       Show full library path instead of just the filename." 1>&2
-			echo "  -h       Print this help message and exit." 1>&2
-			echo "" 1>&2
-			echo "Exit status:" 1>&2
-			echo "  0  success" 1>&2
-			echo "  1  invalid command line option" 1>&2
+			cat <<EOF 1>&2
+Checks which currently running processes use outdated libraries.
+
+Options:
+  -p PID   Only check the process with the given PID. Can be given multiple
+           times, in which case all explicitly given processes are checked.
+  -v       List all outdated libraries for each process.
+  -f       Show full library path instead of just the filename.
+  -h       Print this help message and exit.
+
+Exit status:
+  0  success
+  1  invalid command line option
+  2  severe failure during execution
+EOF
 			exit 0
 			;;
 		:)
-			echo "Error: -${OPTARG} requires an argument" 1>&2
+			echo "Error: -$OPTARG requires an argument" 1>&2
 			exit 1
 			;;
 		*)
-			echo "Error: Invalid option -${OPTARG}"
+			echo "Error: Invalid option -$OPTARG"
 			usage
 			exit 1
 			;;
@@ -64,65 +95,63 @@ then
 fi
 
 # if $pids is empty, fill it with all running PIDs
-if [[ ${#pids[@]} == 0 ]]
+if [[ ${#pids[@]} -eq 0 ]]
 then
-	for pid in $(ps -Ao pid --no-headers)
+	ps_output="$(ps -Ao pid --no-headers)" || fail "couldn't get PID list"
+	for pid in $ps_output
 	do
 		pids+=("$pid")
 	done
 fi
 
-
-# Some helper functions for logging progress
-function progress {
-	echo -n "[$1/$2]" >&2
-}
-function clearLine {
-	echo -en "\r             \r" >&2
-}
-
 # Helper function to print a nice name for a process
-function printProcessName {
+function print_process_name {
 	local pid="$1"
 
-	local exeName
+	local exe_name=""
 	if [[ -f "/proc/$pid/exe" ]]
 	then
-		exeName="$(basename "$(realpath "/proc/$pid/exe")")"
-		exeName="${exeName% (deleted)}" # exe may be deleted, remove the suffix
+		if exe_name="$(realpath --physical "/proc/$pid/exe" 2>/dev/null)"
+		then
+			exe_name="$(basename "${exe_name% (deleted)}")" # exe may be deleted, remove the suffix
+		else
+			warn "couldn't resolve /proc/$pid/exe"
+		fi
 	fi
 
-	local commName
-	commName="$(cat "/proc/$pid/comm")"
+	local comm_name=""
+	comm_name="$(cat "/proc/$pid/comm" 2>/dev/null)" || warn "couldn't read /proc/$pid/comm"
 
-	if [[ -z "$exeName" ]]
+	if [[ -z "$exe_name" ]]
 	then
 		# we only have a comm name
-		echo -ne "\033[0;35m$commName\033[0m (\033[1m$pid\033[0m)"
+		echo -ne "$c_process_name$comm_name$c_reset ($c_pid$pid$c_reset)"
 
-	elif [[ "$exeName" == "$commName"* ]]
+	elif [[ "$exe_name" = "$comm_name"* ]]
 	then
 		# comm name is just a truncated version of exe name
-		echo -ne "\033[0;35m$exeName\033[0m (\033[1m$pid\033[0m)"
+		echo -ne "$c_process_name$exe_name$c_reset ($c_pid$pid$c_reset)"
 
 	else
 		# comm and exe name differ
-		echo -ne "\033[0;35m$commName\033[0m (\033[0;33m$exeName\033[0m, \033[1m$pid\033[0m)"
+		echo -ne "$c_process_name$comm_name$c_reset ($c_process_exe$exe_name$c_reset, $c_pid$pid$c_reset)"
 	fi
 }
 
 # Main part: check all PIDs in $pid
 for (( i=0; i<${#pids[@]}; i++ ))
 do
-	progress $((i+1)) ${#pids[@]}
+	print_progress $((i+1)) ${#pids[@]}
 	pid="${pids[i]}"
 
 	if [[ ! -d "/proc/$pid" ]]
 	then
 		# process already terminated
-		clearLine
+		clear_line
 		continue
 	fi
+
+	[[ -f "/proc/$pid/maps" ]] || warn "/proc/$pid/maps doesn't exist"
 
 	outdated=() # list of all outdated libraries we find
 
@@ -131,10 +160,10 @@ do
 		file="${line% (deleted)}"
 
 		ignore=""
-		for pattern in "${IGNORE_PATTERNS[@]}"
+		for pattern in "${ignore_patterns[@]}"
 		do
 			# shellcheck disable=SC2053
-			if [[ "$file" == $pattern ]]
+			if [[ "$file" = $pattern ]]
 			then
 				ignore="1"
 				break
@@ -162,11 +191,11 @@ do
 
 	if [[ ${#outdated[@]} -gt 0 ]]
 	then
-		clearLine
-		printProcessName "$pid"
+		clear_line
+		print_process_name "$pid"
 		echo -en " uses "
 
-		if [[ ${#outdated[@]} == 1 ]]
+		if [[ ${#outdated[@]} -eq 1 ]]
 		then
 			echo "outdated ${outdated[0]}"
 
@@ -183,5 +212,5 @@ do
 		fi
 	fi
 
-	clearLine
+	clear_line
 done
