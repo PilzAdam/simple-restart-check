3069a6efe94cbd82e9ec7e791697a2fa4d44ff02
PilzAdam <PilzAdam@minetest.net>
2020-07-11 11:21:01 +0200

    Consistently use braces after $

--- simple-restart-check.sh
+++ simple-restart-check.sh
@@ -2,7 +2,7 @@
 
 # A utility script that checks if processes use outdated libraries.
 # This is useful to run after an upgrade, to check which processes need to be restarted.
-# The main idea to is to check in /proc/$pid/maps for entries marked as executable and "(deleted)".
+# The main idea to is to check in /proc/${pid}/maps for entries marked as executable and "(deleted)".
 # See below for a list of 'ignore patterns', for which we don't care if they are marked as "(deleted)"
 
 readonly ignore_patterns=(
@@ -35,14 +35,14 @@ then
 fi
 
 function usage {
-	echo "Usage: $(basename "$0") [-p PID]... [-v] [-f] [-c 0|1] [-h]" 1>&2
+	echo "Usage: $(basename "${0}") [-p PID]... [-v] [-f] [-c 0|1] [-h]" 1>&2
 }
 
 # Helper function to print a colored string
-# $1: string to print
-# $2: color string in the format "\e[XXm"
+# ${1}: string to print
+# ${2}: color string in the format "\e[XXm"
 function cstr {
-	if [[ -z "$use_color" ]]
+	if [[ -z "${use_color}" ]]
 	then
 		echo "${1}"
 	else
@@ -51,26 +51,26 @@ function cstr {
 }
 
 function fail {
-	echo "$(cstr "Error" "$c_error"): $*" 1>&2
+	echo "$(cstr "Error" "${c_error}"): ${*}" 1>&2
 	exit 2
 }
 
 function warn {
-	echo "$(cstr "Warning" "$c_warn"): $*" 1>&2
+	echo "$(cstr "Warning" "${c_warn}"): ${*}" 1>&2
 }
 
 while getopts ":p:vfc:h" opt
 do
-	case "$opt" in
-		p) pids+=("$OPTARG") ;;
+	case "${opt}" in
+		p) pids+=("${OPTARG}") ;;
 		v) verbose="1" ;;
 		f) fullpath="1" ;;
 		c)
-			case "$OPTARG" in
+			case "${OPTARG}" in
 				0) use_color="" ;;
 				1) use_color="1" ;;
 				*)
-					echo "$(cstr "Error" "$c_error"): -$opt expects 0 or 1" 1>&2
+					echo "$(cstr "Error" "${c_error}"): -${opt} expects 0 or 1" 1>&2
 					exit 1
 					;;
 			esac
@@ -97,11 +97,11 @@ EOF
 			exit 0
 			;;
 		:)
-			echo "$(cstr "Error" "$c_error"): -$OPTARG requires an argument" 1>&2
+			echo "$(cstr "Error" "${c_error}"): -${OPTARG} requires an argument" 1>&2
 			exit 1
 			;;
 		*)
-			echo "$(cstr "Error" "$c_error"): Invalid option -$OPTARG" 1>&2
+			echo "$(cstr "Error" "${c_error}"): Invalid option -${OPTARG}" 1>&2
 			usage
 			exit 1
 			;;
@@ -110,68 +110,68 @@ done
 shift $((OPTIND-1))
 
 # check that there are no trailing arguments
-if [[ $# -gt 0 ]]
+if [[ ${#} -gt 0 ]]
 then
-	echo "$(cstr "Error" "$c_error"): Too many arguments" 1>&2
+	echo "$(cstr "Error" "${c_error}"): Too many arguments" 1>&2
 	usage
 	exit 1
 fi
 
-# if $pids is empty, fill it with all running PIDs
+# if ${pids} is empty, fill it with all running PIDs
 if [[ ${#pids[@]} -eq 0 ]]
 then
 	ps_output="$(ps -Ao pid --no-headers)" || fail "couldn't get PID list"
-	for pid in $ps_output
+	for pid in ${ps_output}
 	do
-		pids+=("$pid")
+		pids+=("${pid}")
 	done
 fi
 
 # Helper function to print a nice name for a process
 function print_process_name {
-	local pid="$1"
+	local pid="${1}"
 
 	local exe_name=""
-	if [[ -f "/proc/$pid/exe" ]]
+	if [[ -f "/proc/${pid}/exe" ]]
 	then
-		if exe_name="$(realpath --physical "/proc/$pid/exe" 2>/dev/null)"
+		if exe_name="$(realpath --physical "/proc/${pid}/exe" 2>/dev/null)"
 		then
 			exe_name="$(basename "${exe_name% (deleted)}")" # exe may be deleted, remove the suffix
 		else
-			warn "couldn't resolve /proc/$pid/exe"
+			warn "couldn't resolve /proc/${pid}/exe"
 		fi
 	fi
 
 	local comm_name=""
-	comm_name="$(cat "/proc/$pid/comm" 2>/dev/null)" || warn "couldn't read /proc/$pid/comm"
+	comm_name="$(cat "/proc/${pid}/comm" 2>/dev/null)" || warn "couldn't read /proc/${pid}/comm"
 
-	if [[ -z "$exe_name" ]]
+	if [[ -z "${exe_name}" ]]
 	then
 		# we only have a comm name
-		echo "$(cstr "$comm_name" "$c_name") ($(cstr "$pid" "$c_pid"))"
+		echo "$(cstr "${comm_name}" "${c_name}") ($(cstr "${pid}" "${c_pid}"))"
 
-	elif [[ "$exe_name" = "$comm_name"* ]]
+	elif [[ "${exe_name}" = "${comm_name}"* ]]
 	then
 		# comm name is just a truncated version of exe name
-		echo "$(cstr "$exe_name" "$c_name") ($(cstr "$pid" "$c_pid"))"
+		echo "$(cstr "${exe_name}" "${c_name}") ($(cstr "${pid}" "${c_pid}"))"
 
 	else
 		# comm and exe name differ
-		echo "$(cstr "$comm_name" "$c_name") ($(cstr "$exe_name" "$c_exe"), $(cstr "$pid" "$c_pid"))"
+		echo "$(cstr "${comm_name}" "${c_name}") ($(cstr "${exe_name}" "${c_exe}"), $(cstr "${pid}" "${c_pid}"))"
 	fi
 }
 
-# Main part: check all PIDs in $pid
+# Main part: check all PIDs in ${pids}
 for (( i=0; i<${#pids[@]}; i++ ))
 do
 	echo -en "[$((i+1))/${#pids[@]}]\r" 1>&2
 	pid="${pids[i]}"
 
 	# skip this PID if process is already terminated
-	[[ -d "/proc/$pid" ]] || continue
+	[[ -d "/proc/${pid}" ]] || continue
 
 	# warn if maps file doesn't exist
-	[[ -f "/proc/$pid/maps" ]] || warn "/proc/$pid/maps doesn't exist"
+	[[ -f "/proc/${pid}/maps" ]] || warn "/proc/${pid}/maps doesn't exist"
 
 	outdated=() # list of all outdated libraries we find
 
@@ -183,27 +183,27 @@ do
 		for pattern in "${ignore_patterns[@]}"
 		do
 			# shellcheck disable=SC2053
-			if [[ "$file" = $pattern ]]
+			if [[ "${file}" = ${pattern} ]]
 			then
 				ignore="1"
 				break
 			fi
 		done
 
-		if [[ -z "$ignore" ]]
+		if [[ -z "${ignore}" ]]
 		then
-			if [[ -z "$fullpath" ]]
+			if [[ -z "${fullpath}" ]]
 			then
-				outdated+=("$(basename "$file")")
+				outdated+=("$(basename "${file}")")
 			else
-				outdated+=("$file")
+				outdated+=("${file}")
 			fi
 		fi
 	done < <(
 			# grep filters for files mapped as executable 'x' and ending with 'deleted'
-			# /proc/$pid/maps has 6 columns, delimited by spaces
+			# /proc/${pid}/maps has 6 columns, delimited by spaces
 			# sed removes the first 5, leaving only the filename
-			grep -E '^[^ ]+ ..x.*\(deleted\)$' "/proc/$pid/maps" 2>/dev/null \
+			grep -E '^[^ ]+ ..x.*\(deleted\)$' "/proc/${pid}/maps" 2>/dev/null \
 			| sed -E 's|^[^ ]+ [^ ]+ [^ ]+ [^ ]+ [^ ]+ +||g'                 \
 			| sort                                                           \
 			| uniq
@@ -211,13 +211,13 @@ do
 
 	if [[ ${#outdated[@]} -gt 0 ]]
 	then
-		echo -n "$(print_process_name "$pid") uses "
+		echo -n "$(print_process_name "${pid}") uses "
 
 		if [[ ${#outdated[@]} -eq 1 ]]
 		then
-			echo "outdated $(cstr "${outdated[0]}" "$c_library")"
+			echo "outdated $(cstr "${outdated[0]}" "${c_library}")"
 
-		elif [[ -z "$verbose" ]]
+		elif [[ -z "${verbose}" ]]
 		then
 			echo "multiple outdated libraries"
 
@@ -225,7 +225,7 @@ do
 			echo "multiple outdated libraries:"
 			for lib in "${outdated[@]}"
 			do
-				echo "    $(cstr "$lib" "$c_library")"
+				echo "    $(cstr "${lib}" "${c_library}")"
 			done
 		fi
 	fi
