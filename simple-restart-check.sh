d280a74c73045475d0515c511b9facf09b844d3b
PilzAdam <PilzAdam@minetest.net>
2020-07-15 16:39:36 +0200

    Add license information

--- simple-restart-check.sh
+++ simple-restart-check.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# simple-restart-check.sh
+# Copyright (C) 2020 PilzAdam <pilzadam@gmx.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # A utility script that checks if processes use outdated libraries.
 # This is useful to run after an upgrade, to check which processes need to be restarted.
 # The main idea to is to check in /proc/${pid}/maps for entries marked as executable and "(deleted)".
